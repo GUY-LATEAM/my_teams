c09baf5fd22383d3047babf75228b6cfd62feed3
Lagasse Clément <clement.lagasse@epitech.eu>
2023-04-18 19:37:40 +0100

    Fix Norm

--- server/src/commands/unsubscribe_command/unsubscribe_command.c
+++ server/src/commands/unsubscribe_command/unsubscribe_command.c
@@ -11,6 +11,5 @@ int unsubscribe_command(__attribute__((unused)) void *user_data,
 __attribute__((unused)) void *protocol_data, __attribute__((unused)) char *args,
 __attribute__((unused)) circular_buffer_t *write_buffer)
 {
-    
     return EXIT_SUCCESS;
 }
